7a9d1acf546f6dabdd6b316709d062e2f278461a
Devraj Kumar <devraj.kumar@gocomet.com>
2024-04-02 16:22:36 +0530

    extra comments removed

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,24 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
 
-  # GET /books
+ 
   def index
     @books = Book.select { |book| book.quantity - book.borrowings.count > 0 }
   end
 
-  # GET /books/1
+
   def show
   end
 
-  # GET /books/new
+
   def new
     @book = Book.new
   end
 
-  # GET /books/1/edit
   def edit
   end
 
-  # POST /books
+
   def create
     @book = Book.new(book_params)
 
@@ -30,7 +29,7 @@ class BooksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /books/1
+
   def update
     if @book.update(book_params)
       redirect_to @book, notice: 'Book was successfully updated.'
@@ -44,19 +43,17 @@ class BooksController < ApplicationController
     redirect_to borrowings_url, notice: 'Book returned successfully.'
   end
 
-  # DELETE /books/1
+
   def destroy
     @book.destroy
     redirect_to books_url, notice: 'Book was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_book
       @book = Book.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def book_params
       params.require(:book).permit(:title, :quantity)
     end
--- app/controllers/borrowings_controller.rb
+++ app/controllers/borrowings_controller.rb
@@ -48,9 +48,6 @@ class BorrowingsController < ApplicationController
     end
   end
 
-
-
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_borrowing
