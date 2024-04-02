434eea58e6ac2397a23d49f4ced72dc9f32cd8bb
Devraj Kumar <devraj.kumar@gocomet.com>
2024-04-02 16:03:12 +0530

    all assignment specs added

--- spec/controllers/borrowings_controller_spec.rb
+++ spec/controllers/borrowings_controller_spec.rb
@@ -9,4 +9,38 @@ RSpec.describe BorrowingsController, type: :controller do
       expect(Borrowing.count).to eq(1)
     end
   end
+
+  describe 'POST #create' do
+    it 'tries to create multiple borring of same book' do
+      user = User.create(name: 'John Doe')
+      book = Book.create(title: 'Book 1', quantity: 2)
+      post :create, params: { borrowing: { user_id: user.id, book_id: book.id } }
+      post :create, params: { borrowing: { user_id: user.id, book_id: book.id } }
+      expect(Borrowing.count).to eq(1)
+    end
+  end
+
+  describe 'POST #create' do
+    it 'tries to create multiple borring of different book' do
+      user = User.create(name: 'John Doe')
+      book = Book.create(title: 'Book 1', quantity: 1)
+      book2 = Book.create(title: 'Book 2', quantity: 1)
+      post :create, params: { borrowing: { user_id: user.id, book_id: book.id } }
+      post :create, params: { borrowing: { user_id: user.id, book_id: book2.id } }
+      expect(Borrowing.count).to eq(2)
+    end
+  end
+
+  describe 'POST #create' do
+    it 'tries to create more than 2 borring of different book' do
+      user = User.create(name: 'John Doe')
+      book = Book.create(title: 'Book 1', quantity: 1)
+      book2 = Book.create(title: 'Book 2', quantity: 1)
+      book3 = Book.create(title: 'Book 3', quantity: 1)
+      post :create, params: { borrowing: { user_id: user.id, book_id: book.id } }
+      post :create, params: { borrowing: { user_id: user.id, book_id: book2.id } }
+      post :create, params: { borrowing: { user_id: user.id, book_id: book3.id } }
+      expect(Borrowing.count).to eq(2)
+    end
+  end
 end
