2de7794c9309c5e98888afd9f059838e12032a8a
Devraj Kumar <devraj.kumar@gocomet.com>
2024-04-02 16:19:41 +0530

    spec written better with use of factories

--- spec/controllers/borrowings_controller_spec.rb
+++ spec/controllers/borrowings_controller_spec.rb
@@ -1,46 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe BorrowingsController, type: :controller do
-  describe 'POST #create' do
-    it 'creates a new borrowing' do
-      user = User.create(name: 'John Doe')
-      book = Book.create(title: 'Book 1', quantity: 1)
-      post :create, params: { borrowing: { user_id: user.id, book_id: book.id } }
-      expect(Borrowing.count).to eq(1)
-    end
-  end
+  let(:user) { create(:user, name: 'John Doe') }
+  let(:book) { create(:book, title: 'Book 1', quantity: 2) }
+  let(:book2) { create(:book, title: 'Book 2', quantity: 1) }
+  let(:book3) { create(:book, title: 'Book 3', quantity: 1) }
 
   describe 'POST #create' do
-    it 'tries to create multiple borring of same book' do
-      user = User.create(name: 'John Doe')
-      book = Book.create(title: 'Book 1', quantity: 2)
-      post :create, params: { borrowing: { user_id: user.id, book_id: book.id } }
-      post :create, params: { borrowing: { user_id: user.id, book_id: book.id } }
-      expect(Borrowing.count).to eq(1)
-    end
-  end
+    context 'when borrowing a single book' do
 
-  describe 'POST #create' do
-    it 'tries to create multiple borring of different book' do
-      user = User.create(name: 'John Doe')
-      book = Book.create(title: 'Book 1', quantity: 1)
-      book2 = Book.create(title: 'Book 2', quantity: 1)
-      post :create, params: { borrowing: { user_id: user.id, book_id: book.id } }
-      post :create, params: { borrowing: { user_id: user.id, book_id: book2.id } }
-      expect(Borrowing.count).to eq(2)
+      it 'creates a new borrowing' do
+        post :create, params: { borrowing: { user_id: user.id, book_id: book.id } }
+        expect(Borrowing.count).to eq(1)
+      end
     end
-  end
 
-  describe 'POST #create' do
-    it 'tries to create more than 2 borring of different book' do
-      user = User.create(name: 'John Doe')
-      book = Book.create(title: 'Book 1', quantity: 1)
-      book2 = Book.create(title: 'Book 2', quantity: 1)
-      book3 = Book.create(title: 'Book 3', quantity: 1)
-      post :create, params: { borrowing: { user_id: user.id, book_id: book.id } }
-      post :create, params: { borrowing: { user_id: user.id, book_id: book2.id } }
-      post :create, params: { borrowing: { user_id: user.id, book_id: book3.id } }
-      expect(Borrowing.count).to eq(2)
+    context 'when trying to borrow multiple copies of the same book' do
+
+      it 'creates only one borrowing' do
+        post :create, params: { borrowing: { user_id: user.id, book_id: book.id } }
+        post :create, params: { borrowing: { user_id: user.id, book_id: book.id } }
+        expect(Borrowing.count).to eq(1)
+      end
+    end
+
+    context 'when trying to borrow different books' do
+      it 'creates multiple borrowings' do
+        post :create, params: { borrowing: { user_id: user.id, book_id: book.id } }
+        post :create, params: { borrowing: { user_id: user.id, book_id: book2.id } }
+        expect(Borrowing.count).to eq(2)
+      end
+    end
+
+    context 'when trying to borrow more than 2 different books' do
+      it 'creates only two borrowings' do
+        post :create, params: { borrowing: { user_id: user.id, book_id: book.id } }
+        post :create, params: { borrowing: { user_id: user.id, book_id: book2.id } }
+        post :create, params: { borrowing: { user_id: user.id, book_id: book3.id } }
+        expect(Borrowing.count).to eq(2)
+      end
     end
   end
 end
---
+++ spec/factories/books.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :book do
+    title { 'Book 1' }
+    quantity { 1 }
+  end
+end
